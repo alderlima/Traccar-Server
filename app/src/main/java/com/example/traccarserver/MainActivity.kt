a1b3efa7bf43d239f4e69f67ed5990bb76e96584
alderlima <aldemice@gmail.com>
2026-04-29 09:06:30 -0300

    Fix: null safety em ServerService

--- app/src/main/java/com/example/traccarserver/MainActivity.kt
+++ app/src/main/java/com/example/traccarserver/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.webkit.WebView
 import android.webkit.WebViewClient
