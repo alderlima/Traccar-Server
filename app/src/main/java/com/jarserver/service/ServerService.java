e8eecf7876ea60d1f8ac18f86336e86f955d7197
alderlima <aldemice@gmail.com>
2026-04-29 00:46:59 -0300

    Fix: null safety em ServerService

--- app/src/main/java/com/jarserver/service/ServerService.java
+++ app/src/main/java/com/jarserver/service/ServerService.java
@@ -154,18 +154,10 @@ public class ServerService extends Service {
         pb.redirectErrorStream(true);
         jarProcess = pb.start();
 
-        // Substitua a linha 157 por algo assim:
-        int pid = -1;
-        try {
-            java.lang.reflect.Field f = jarProcess.getClass().getDeclaredField("pid");
-            f.setAccessible(true);
-            pid = f.getInt(jarProcess);
-        } catch (Exception e) {
-            pid = -1;
-        }
-        
-        notifyLog("Servidor iniciado com PID: " + pid);
-
+        // Obter PID do processo (Java 9+)
+        long pid = jarProcess.pid();
+        notifyLog("Servidor Traccar iniciado com PID: " + pid);
+        notifyLog("Acesse a interface web em: http://localhost:8082");
         notifyServerStarted();
 
         // Ler output do processo
